8808b3503a4c959ac3ad523d76dd4b79d1493c6a
cai <qbao1607@gmail.com>
2025-10-01 02:45:44 +0700

    update title

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 window.onload = () => {
-    // 移除container类以启动动画
     const container = document.querySelector('.container');
     if (container) {
         container.classList.remove("container");
